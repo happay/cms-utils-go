7d30467ed49a281a554842f54daa572dce7b8f5d
RAHUL KAUSHIK <rahul.kaushik@Rahul-Kaushik-MacBook-Air.local>
2022-11-22 12:13:47 +0530

    changes

--- util/jwt.go
+++ util/jwt.go
@@ -1,67 +1,34 @@
 package util
 
 import (
-
-	"fmt"
 	"time"
-	"log"
-   
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
+
 	"github.com/asaskevich/govalidator"
 
 	"gorm.io/gorm"
-	"os"
 
 	"github.com/dgrijalva/jwt-go"
 
 	"golang.org/x/crypto/bcrypt"
 )
-type Admin struct {
 
+type Admin struct {
 	gorm.Model
-	
-	FirstName string `json:"firstName" valid:"required~FirstName is required , length(1|64)~Invalid FirstName"`
-	MiddleName  string `json:"middleName" `
-	LastName  string `json:"lastName" valid:"required~LastName is required , length(1|64)~Invalid LastName"`
-	Email    string `json:"email" valid:"required~Email is required , length(1|20)~Invalid Email"`
-	Password string `json:"password" valid:"required~Password is required , length(1|40)~Empty Password"`
-	Phone    string `json:"phone" valid:"required~Phone is required , length(1|15)~Invalid Phone"`
-	
-	
+
+	FirstName  string `json:"firstName" valid:"required~FirstName is required , length(1|64)~Invalid FirstName"`
+	MiddleName string `json:"middleName" `
+	LastName   string `json:"lastName" valid:"required~LastName is required , length(1|64)~Invalid LastName"`
+	Email      string `json:"email" valid:"required~Email is required , length(1|20)~Invalid Email"`
+	Password   string `json:"password" valid:"required~Password is required , length(1|40)~Empty Password"`
+	Phone      string `json:"phone" valid:"required~Phone is required , length(1|15)~Invalid Phone"`
 }
 
-func Init() *gorm.DB {
-	err := godotenv.Load("jwt.env")
-	if err != nil {
-		log.Fatalf("Some error occured. Err: %s", err)
-	}
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-	databaseName := os.Getenv("DB_NAME")
+func AutoMigrate(db *gorm.DB) {
 
-	//dbURL := "host= user=postgres password=harshit24; dbname=virtual_account_service port=5432 sslmode=disable"
-	dbURL := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, databaseName, port)
-
-	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
-
-	if err != nil {
-		panic("Oops! Couldn't connect to the Database.")
-	}
-
-	fmt.Printf("success! DB connected! \n")
-
-	err = db.AutoMigrate(&Admin{})
+	err := db.AutoMigrate(&Admin{})
 	if err != nil {
 		panic(err)
 	}
-
-
-
-
-	return db
 }
 
 func HashPassword(password string) (string, error) {
@@ -72,30 +39,30 @@ func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
-type EmailAlreadyPresentError struct {	
+
+type EmailAlreadyPresentError struct {
 }
-func (m * EmailAlreadyPresentError) Error() string{
-    return "User with same email already exists"
+
+func (m *EmailAlreadyPresentError) Error() string {
+	return "User with same email already exists"
 }
-type PhoneAlreadyPresentError struct {}
-func (m *PhoneAlreadyPresentError) Error() string{
+
+type PhoneAlreadyPresentError struct{}
+
+func (m *PhoneAlreadyPresentError) Error() string {
 	return "User with same phone already exists"
 }
 
+func Signup(admin Admin, db *gorm.DB) error {
 
-func Signup(admin Admin)( error) {
-	db := Init()
-
-	
 	var admins []Admin
 	var Admins []Admin
-	
 
 	_, err := govalidator.ValidateStruct(admin)
 	if err != nil {
 		return err
 	}
-	
+
 	Encrypted_Password, err := HashPassword(admin.Password)
 	if err != nil {
 
@@ -103,32 +70,25 @@ func Signup(admin Admin)( error) {
 	}
 	admin.Password = Encrypted_Password
 
-	
-
 	db.Where("Email= ?", &admin.Email).Find(&admins)
-	
+
 	for i := range admins {
 		if admin.Email == admins[i].Email {
-			
-			
+
 			return &EmailAlreadyPresentError{}
 		}
 	}
 
 	db.Where("Phone=?", &admin.Phone).Find(&Admins)
 
-
 	for i := range Admins {
 		if admin.Phone == Admins[i].Phone {
-			
+
 			return &PhoneAlreadyPresentError{}
 		}
 	}
 
-
 	db.Create(&admin)
-
-	
 
 	return nil
 
@@ -139,22 +99,20 @@ type WrongPasswordError struct{}
 func (m *WrongPasswordError) Error() string {
 	return "Invalid Password"
 }
-func Login(email,password string ) (error ,map[string]string) {
-	db := Init()
+func Login(email, password string, db *gorm.DB) (error, map[string]string) {
+
 	result := make(map[string]string)
 	var db_Admin Admin
-	
 
 	pass1 := password
-	
 
 	db.Where("email = ?", email).First(&db_Admin)
 
 	expectedPassword := db_Admin.Password
 
 	if !CheckPasswordHash(pass1, expectedPassword) {
-		
-		return &WrongPasswordError{},nil
+
+		return &WrongPasswordError{}, nil
 	}
 
 	expirationTime := time.Now().Add(time.Minute * 30)
@@ -169,18 +127,15 @@ func Login(email,password string ) (error ,map[string]string) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	tokenString, err := token.SignedString(jwt_key)
 	if err != nil {
-		
-		return err,nil
-	}
 
-	
+		return err, nil
+	}
 
 	result["token"] = tokenString
 	result["expires"] = expirationTime.String()
 
-	return nil,result
+	return nil, result
 }
-
 
 type Claims struct {
 	Email    string `json:"email"`
