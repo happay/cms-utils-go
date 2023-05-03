10c1feb1fb2bd9f654da7d25a634843af3f1aa63
aniruddha.nath <aniruddha.nath@happay.com>
2023-05-03 20:21:52 +0530

    added a new logger with log rotation

--- logger/logrus.go
+++ logger/logrus.go
@@ -40,7 +40,50 @@ type LogInitializerObject struct {
 	S3Config      *aws.Config
 }
 
-func initializeLoggerV2(logInitializerObject LogInitializerObject) {
+func initializeLoggerV2(logPrefix, logPath, appName string) {
+	logDirSplit := strings.Split(logPath, "/")
+	var logDirSlice []string
+	for i := 0; i < len(logDirSplit)-1; i++ {
+		logDirSlice = append(logDirSlice, logDirSplit[i])
+	}
+	logDir := strings.Join(logDirSlice, "/")
+	logFileName := logDirSplit[len(logDirSplit)-1]
+
+	if _, err := os.Stat(logDir); os.IsNotExist(err) {
+		err := os.MkdirAll(logDir, os.FileMode(GenerateDirectoryPermissionMode))
+		if err != nil {
+			reason := fmt.Sprintf("create directory %s failed: %s", logDir, err)
+			err = errors.New(reason)
+			fmt.Println(err)
+			return
+		}
+	}
+	txnLogFile := logDir + "/" + logFileName + ".log"
+	if file, err := os.OpenFile(txnLogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		os.FileMode(FileWritePermissionMode)); err != nil {
+		panic(err)
+	} else {
+		logWriter = io.MultiWriter(file)
+		hook := GrayLogHook(appName)
+		defer hook.Flush()
+		loggerV2 = logrus.New()
+
+		loggerV2.SetFormatter(&logrus.JSONFormatter{})
+		loggerV2.SetOutput(logWriter)
+		//set file name and line number
+		loggerV2.SetReportCaller(true)
+		loggerV2.AddHook(hook)
+
+		// Set log level
+		loggerV2.SetLevel(logrus.DebugLevel)
+
+		// This will disable logging to stdout
+		loggerV2.Out = io.Discard
+
+	}
+}
+
+func initializeLoggerWithLogRotation(logInitializerObject LogInitializerObject) {
 
 	logPath := logInitializerObject.LogPath
 	logS3FilePath := logInitializerObject.LogS3FilePath
@@ -203,9 +246,16 @@ func IsFileExists(key, bucketName string, s3Config *aws.Config) (exists bool) {
 // - logPrefix - it is a string used as Prefix on each log line
 // - logPath - absolute path of the log file where the logs will be written
 // - appName - It is app Name, from which service this function is being called to route the log to a specific Graylog stream.
-func GetLoggerV2(logInitializerObject LogInitializerObject) *logrus.Logger {
+func GetLoggerV2(logPrefix, logPath, appName string) *logrus.Logger {
 	logInit.Do(func() {
-		initializeLoggerV2(logInitializerObject)
+		initializeLoggerV2(logPrefix, logPath, appName)
+	})
+	return loggerV2
+}
+
+func GetLoggerWithLogRotation(logInitializerObject LogInitializerObject) *logrus.Logger {
+	logInit.Do(func() {
+		initializeLoggerWithLogRotation(logInitializerObject)
 	})
 	return loggerV2
 }
