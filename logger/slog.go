faef0fce1aab78c3aac571571e2d0590d348095e
shashank-happay <113413704+shashank-happay@users.noreply.github.com>
2023-09-28 16:26:17 +0530

    SLog implementation (#32)
    * SLog implemented
    
    * changes in common method for slog logging
    
    ---------
    
    Co-authored-by: Deepika Azad <deepikaazad@HAPPAYBLRMAC1605.local>

---
+++ logger/slog.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"context"
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
+// ============ Internal(private) Methods - can only be called from inside this package ==============
+
+type ContextReqId struct{}
+type ContextAppId struct{}
+
+func initializeLoggerV3() *slog.Logger {
+	enc := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	})
+	h := ContextHandler{enc, []any{
+		ContextReqId{},
+		ContextAppId{},
+	}}
+	return slog.New(h)
+}
+
+// =========== Exposed (public) Methods - can be called from external packages ============
+
+// GetLogger returns the slog logger object.
+func GetLoggerV3() *slog.Logger {
+	return initializeLoggerV3()
+}
+
+type ContextHandler struct {
+	slog.Handler
+	keys []any
+}
+
+func (h ContextHandler) Handle(ctx context.Context, r slog.Record) error {
+	r.AddAttrs(h.observe(ctx)...)
+	return h.Handler.Handle(ctx, r)
+}
+
+func (h ContextHandler) observe(ctx context.Context) (as []slog.Attr) {
+	for _, k := range h.keys {
+		a, ok := ctx.Value(k).(slog.Attr)
+		if !ok {
+			continue
+		}
+		a.Value = a.Value.Resolve()
+		as = append(as, a)
+	}
+	return
+}
