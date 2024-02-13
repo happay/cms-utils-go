cd56b05692094bf767f04e72b842ae185427a7d9
Deepika Azad <deepikaazad@ip-192-168-1-3.ap-south-1.compute.internal>
2024-02-13 18:00:12 +0530

    Added global span tracer

--- tracing/opentelemetery.go
+++ tracing/opentelemetery.go
@@ -1,6 +1,9 @@
 package tracing
 
 import (
+	"fmt"
+
+	"github.com/happay/cms-utils-go/v2/logger"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
 )
@@ -11,6 +14,13 @@ func (tp *DataDogProvider) InitTracing() {
 	otel.SetTracerProvider(tp.TracerProvider)
 	p := NewPropagator()
 	otel.SetTextMapPropagator(p)
+	defer func() {
+		if r := recover(); r != nil {
+			err := fmt.Errorf("error rendering template: %s", r)
+			logger.GetLoggerV3().Error(err.Error())
+			panic(r)
+		}
+	}()
 }
 
 // NewPropagator, return the TextMapPropogator which allows distributed tracing
